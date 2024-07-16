f920ac6c521c8f9a626f4deb0f484482afc822c7
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 17:14:00 +0530

    fixing

--- ec-module.tf
+++ ec-module.tf
@@ -2,9 +2,9 @@ module "ec2_instance" {
     source = "./modules"
     ami = "ami-0a0e5d9c7acc336f1"
     instance_type ="t2.micro"
-    vpc_security_group_ids = sg-0ab80f7652fa6e7e6
+    vpc_security_group_ids = ["sg-0ab80f7652fa6e7e6"]
 
-    tag {
+    tags {
         Name="web"
     }
   
