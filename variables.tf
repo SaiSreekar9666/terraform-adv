04fed0a71d5e8c46b60d3e646d5ef214fe47986a
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 18:02:40 +0530

    senistive

--- variables.tf
+++ variables.tf
@@ -5,6 +5,7 @@ variable "aws_access_key" {
 variable "aws_sceret_key" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
+  sensitive = true
 }
 variable "region" {
   description = "Value of the Name tag for the EC2 instance"
