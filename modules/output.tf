c2cade09a7b6c015025be5bf769fa84789ba3ae0
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 18:04:54 +0530

    value fixing

--- modules/output.tf
+++ modules/output.tf
@@ -1,6 +1,6 @@
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web.public_ip
+  value       = aws_instance.var.Name.public_ip
 }
 
