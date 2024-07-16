bd3335c882fa90d3cbf0b98ce4e2ef9abc48f132
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 17:24:06 +0530

    s

--- modules/main.tf
+++ modules/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name = "users"
-  vpc_security_group_ids=[sg-0ab80f7652fa6e7e6]
+  vpc_security_group_ids = ["sg-0ab80f7652fa6e7e6"]
   tags = {
     Name = "web"
   }
