a1139219bed25976b5790a120a038f47845ea9b4
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 16:21:40 +0530

    akajsdflkasjdlkfs

--- modules/variables.tf
+++ modules/variables.tf
@@ -14,7 +14,3 @@ variable "ami" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string  
 }
-variable "instance_type" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string  
-}
