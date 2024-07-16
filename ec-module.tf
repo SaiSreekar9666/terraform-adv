22d84c12a450fa6049b5a0f5511d9cf162b0950b
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 16:07:55 +0530

    fix

--- ec-module.tf
+++ ec-module.tf
@@ -1,5 +1,5 @@
 module "ec2_instance" {
-    source = ".terraform-adv/module"
+    source = "./terraform-adv/module"
     ami = ami-0a0e5d9c7acc336f1
     instance_type ="t2.medium"
     vpc_security_group_ids =[sg-0ab80f7652fa6e7e6]
