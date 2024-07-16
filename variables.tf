d4a7139a30b5c9e799153b97bd5c41bf822e5d5b
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 16:47:16 +0530

    fixing

--- variables.tf
+++ variables.tf
@@ -10,5 +10,14 @@ variable "region" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
 }
+variable "ami" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
 
+}
+variable "instance_type" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+
+}
 
