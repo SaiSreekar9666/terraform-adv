95f8502b3062539ae02a15a2981cd9db43f133e1
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 13:52:25 +0530

    some changes

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -2,11 +2,14 @@
 module "ec2_instance" {
   source  = "./module"
 
-  name = "single-instance"
-  ami = var.ami_id
 
-  instance_type          = var.instance_type
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
   key_name               = "users"
   vpc_security_group_ids = ["sg-0ab80f7652fa6e7e6"]
 
+  tags = {
+    Name = "instance-web"
+}
 }
