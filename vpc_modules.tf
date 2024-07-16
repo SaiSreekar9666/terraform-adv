44290f2207aca50ab0503318c9c13cbf10d0ae09
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 13:08:53 +0530

    sg

--- vpc_modules.tf
+++ vpc_modules.tf
@@ -1,20 +1,10 @@
-module "vpc" {
-  source = "./module"
-  cidr = "10.0.0.0/16"
-  vpc_name= "ibm_vpc"
-  vpc_id = "ibm_vpc"
-  public_subnet_cidr_block  = "10.0.0.0/16"
-  app_subnet_cidr_block = "10.0.16.0/16"
-  db_subnet_cidr_block = "10.0.27.0/16"
+#ec2_instance
 
-
-
-}
 module "ec2_instance" {
   source  = "./module"
   instance_type          = "t2.micro"
   key_name               = "users"
-  vpc_security_group_ids = ["sg-12345678"]
+  vpc_security_group_ids = [sg-0ab80f7652fa6e7e6]
   subnet_id              = "ibm_web" 
     
 }
