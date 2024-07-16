9377bf37ac20148a02bcaf986ff59617ddaec6ee
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 16:40:26 +0530

    finxing

--- modules/variables.tf
+++ modules/variables.tf
@@ -10,4 +10,13 @@ variable "region" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
 }
+variable "ami" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+}
+variable "instance_type" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+}
+
 
--- variables.tf
+++ variables.tf
@@ -10,14 +10,5 @@ variable "region" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
 }
-variable "instance_type" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  
-}
-variable "ami" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  
-}
+
 
