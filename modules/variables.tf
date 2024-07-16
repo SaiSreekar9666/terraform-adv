c90da8aeed0a31032e665c665afca81c56372cbf
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 16:50:51 +0530

    fixing

--- modules/variables.tf
+++ modules/variables.tf
@@ -1,14 +1,12 @@
-variable "aws_access_key" {
+#klhfakjhf
+variable "ami" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-}
-variable "aws_sceret_key" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-}
-variable "region" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-}
 
+}
+variable "instance_type" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+
+}
 
