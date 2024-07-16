e5bbf7e9338dc3714b1740ea0d9c3fcfe578d7fd
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 17:03:39 +0530

    source

--- ec-module.tf
+++ ec-module.tf
@@ -1,5 +1,5 @@
 module "ec2_instance" {
-    source = "./module"
+    source = "./module/ec2_instance"
     ami = "ami-0a0e5d9c7acc336f1"
     instance_type ="t2.micro"
     vpc_security_group_ids =[sg-0ab80f7652fa6e7e6]
