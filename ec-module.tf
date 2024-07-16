7ec70d284de1ad44d30f4d29c9a81500a6c95561
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 16:10:18 +0530

    source error

--- ec-module.tf
+++ ec-module.tf
@@ -1,5 +1,5 @@
 module "ec2_instance" {
-    source = "./terraform-adv/module"
+    source = "./module/ec2-instance"
     ami = ami-0a0e5d9c7acc336f1
     instance_type ="t2.medium"
     vpc_security_group_ids =[sg-0ab80f7652fa6e7e6]
