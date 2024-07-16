b1d7459de7943f80f55a86a94d42cb653f8cf4ae
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 18:13:09 +0530

    ss

--- modules/output.tf
+++ modules/output.tf
@@ -1,6 +1,6 @@
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.Name.public_ip
+  value       = aws_instance.web.public_ip
 }
 
