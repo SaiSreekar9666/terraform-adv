a9d87c883ea991c5471cacaa9dda31093f92b397
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 20:48:44 +0530

    count provider and variables

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "access_key" {
+    description = "value for the access key"
+    type= string
+  
+}
+variable "secret_key" {
+    description = " value for the secret key"
+    type=string
+  
+}
