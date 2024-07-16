770bb15b73f5ca4440f6603a1ab2737063a6c0d6
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 13:10:05 +0530

    sourcce

--- vpc_modules.tf
+++ vpc_modules.tf
@@ -1,7 +1,7 @@
 #ec2_instance
 
 module "ec2_instance" {
-  source  = "./module"
+  source  = "./module/terrform-adv/"
   instance_type          = "t2.micro"
   key_name               = "users"
   vpc_security_group_ids = [sg-0ab80f7652fa6e7e6]
