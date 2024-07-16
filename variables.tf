7991bdf11bcc94a0ca5ce522f1cb50f10f28afe7
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-16 16:05:53 +0530

    var

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "aws_access_key" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+}
+variable "aws_sceret_key" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+}
+variable "region" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+}
+variable "ami" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string  
+}
+variable "instance_type" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string  
+}
