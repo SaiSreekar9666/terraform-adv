4852d6a69020ecb40178d485898dc5596ac17b57
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-17 17:42:55 +0530

    chnages

--- modules/output.tf
+++ modules/output.tf
@@ -1,10 +1,10 @@
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web.public_ip
+  value       = aws_instance.web_public_ip
 }
 output "key_name" {
   description = "key-name"
-  value = aws_instance.web.key_name
+  value = aws_instance.web_key_name
   
 }
