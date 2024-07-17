37a9da6a4804532ffa8d47ceda2b6e562cd51afd
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-17 18:30:18 +0530

    changes to ouput

--- modules/output.tf
+++ modules/output.tf
@@ -8,3 +8,8 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.web.public_ip
 }
+output "my_console_ouput" {
+  value = "hello this the ouput"
+
+  
+}
