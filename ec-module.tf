46664cbd0854fb9bad7b6bccc53182d80484e762
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-17 17:09:17 +0530

    changes

--- ec-module.tf
+++ ec-module.tf
@@ -4,6 +4,6 @@ module "ec2_instance" {
     Name = "web"
     ami = "ami-0a0e5d9c7acc336f1"
     instance_type = "t2.medium"
-    vpc_security_group_ids = [sg-0ab80f7652fa6e7e6]
+    vpc_security_group_ids = "sg-0ab80f7652fa6e7e6"
 
 }
