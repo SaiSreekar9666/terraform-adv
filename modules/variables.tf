4a3bc230cfe6b6bba3432cb93f9ec63317c23e18
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-17 15:10:21 +0530

    vpc

--- modules/variables.tf
+++ modules/variables.tf
@@ -19,4 +19,86 @@ variable "Name" {
     type= string
   
 }
+variable "vpc_name" {
+  description = "value for the vpc-name"
+  type=string
+  
+}
+variable "ibm_public_sn" {
+  description = "value for the public-name"
+  type=string
+
+  
+}
+variable "ibm_private_sn" {
+  description = "value for the private-name"
+  type=string
+
+  
+}
+variable "ibm_app_sn" {
+  description = "value for the app-sn"
+  type = string
+  
+}
+variable "ibm_gw" {
+  description = "value for the ibm-gw"
+  
+}
+variable "ibm_nat" {
+  description = "value for the ibm-nat"
+  
+}
+variable "ibm_eip" {
+  description = "value for the ibm-eip"
+  
+}
+variable "ibm_app_rt" {
+  description = "value for the app-route-table"
+  type=string
+  
+}
+variable "ibm_web_rt" {
+  description = "value for the web-rt"
+  type=string
+  
+}
+variable " ibm_db_rt" {
+  description = "value for the db-rt"
+  type=string
+  
+}
+variable "ibm_app_nacl" {
+  description = "value for the app-nacl"
+  type=string
+  
+}
+variable "ibm_db_nacl" {
+  description = "value for the db-nacl"
+  type=string
+  
+}
+variable "ibm_web_nacl" {
+  description = "value for the app-nacl"
+  type=string
+  
+}
+variable "ibm_app_sg" {
+  description = "value for the app-sg"
+  type=string
+  
+}
+variable "ibm_db_sg" {
+  description = "value for the db-sg"
+  type=string
+  
+}
+variable "ibm_web_sg" {
+  description = "value for the web-sg"
+  type=string
+}
+
+
+
+
 
---
+++ vpc.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+    source = "./module"
+    cidr_block = "10.0.0.0/16"
+    
+  
+}
