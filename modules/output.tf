30b67d166c9baa123f7b22bd5abb08a16d1ca8e2
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-17 18:07:05 +0530

    output

--- modules/output.tf
+++ modules/output.tf
@@ -1,7 +1,7 @@
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = module.web.public_ip
+  value       = module.web.instance_public_ip
 }
 output "key_name" {
   description = "key-name"
