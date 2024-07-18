a46b3f48800f48160f80753aa2f716ada6496ab7
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-18 16:22:24 +0530

    changes to ouput

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "instance_public_ip" {
-  value = module.instance.instance_public_ip
+  value = module.aws_instance.instance_public_ip
 }
 
 output "instance_id" {
-  value = module.instance.instance_id
+  value = module.aws_instance.instance_id
 }
