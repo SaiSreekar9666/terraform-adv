9dd495f1ce6ec451473c26a85cea5c723816af78
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-18 16:01:23 +0530

    changes

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_access_key" {
-    description = "value for the access key"
-    type= string
+    description = "value for the aws-access-key"
+    type = string
   
 }
 variable "aws_secret_key" {
-    description = " value for the secret key"
+    description = "value for the secret-key"
     type=string
   
 }
