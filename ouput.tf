07de56200139221b3f5858659e27eeb0e2c244c8
saisreekar9666 <gsaisreekar9666@gmail.com>
2024-07-18 16:11:33 +0530

    output instance names

--- ouput.tf
+++ ouput.tf
@@ -1,3 +1,6 @@
 output "instance_public_ips" {
   value = [for instance in aws_instance.count : instance.public_ip]
 }
+output "instance_names" {
+  value = [for instance in aws_instance.count : instance.tags["Name"]]
+}
